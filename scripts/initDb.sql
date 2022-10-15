ca56cef9bd55726e53dccf9ad5f6df07bfe260da
Franciszek Gorczyca <franciszekgorczyca@sztarbuks.local>
2022-10-15 15:25:18 +0200

    Database scripts

---
+++ scripts/initDb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cases (id int, case_state int, department_id int, judge_id int);
+CREATE TABLE documents (id int, case_id int, received_on timestamp, sender varchar, description varchar, raw_data bytea);
+CREATE TABLE departments (id int, department_name varchar);
+CREATE TABLE judges (id int, department_id int, full_name varchar);
+
+CREATE SEQUENCE departments_sequence START 1;
+CREATE SEQUENCE judges_sequence START 1;
