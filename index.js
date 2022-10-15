5c0618b2c0321652e47acd3853110cf4e611ea08
Franciszek Gorczyca <franciszekgorczyca@sztarbuks.local>
2022-10-16 00:09:52 +0200

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const getCasesForJudge = require('./controllers/judgesController')
 const GetMetaData = require('./controllers/readmetaController')
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json())
 
